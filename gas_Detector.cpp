c24f6c8c1fb47b3d77e5df2e1497b7046e75e70e
Nischal <nischalshrestha800@gmail.com>
2024-03-08 11:29:52 +0545

    first commit

---
+++ gas_Detector.cpp
@@ -0,0 +1,30 @@
+int buzzer = D2;
+int smokeA0 = A0;
+
+// Your threshold value. You might need to change it.
+int sensorThres = 600;
+
+void setup()
+{
+    pinMode(buzzer, OUTPUT);
+    pinMode(smokeA0, INPUT);
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    int analogSensor = analogRead(smokeA0);
+
+    Serial.print("Pin A0: ");
+    Serial.println(analogSensor);
+    // Checks if it has reached the threshold value
+    if (analogSensor > sensorThres)
+    {
+        tone(buzzer, 1000, 200);
+    }
+    else
+    {
+        noTone(buzzer);
+    }
+    delay(100);
+}
